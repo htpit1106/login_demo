20a100bb627a13e59cbc78b25d852271c4f10076
phuong <phuonggm204@gmail.com>
2026-04-16 09:39:17 +0700

    feat: enhance login functionality with status handling and user locking mechanism

--- lib/features/auth/login/login_state.dart
+++ lib/features/auth/login/login_state.dart
@@ -1,24 +1,29 @@
 import 'package:equatable/equatable.dart';
 import 'package:login_demo/core/data/model/enums/load_status.dart';
+import 'package:login_demo/core/data/model/enums/login_status.dart';
 
 class LoginState extends Equatable {
   final LoadStatus loadLoginStatus;
+  final LoginStatus loginStatus;
   final bool isSubmit;
   final bool isLoginCountLimit;
 
   const LoginState({
     this.loadLoginStatus = LoadStatus.initial,
+    this.loginStatus = LoginStatus.initial,
     this.isSubmit = false,
     this.isLoginCountLimit = false,
   });
 
   LoginState copyWith({
     LoadStatus? loadLoginStatus,
+    LoginStatus? loginStatus,
     bool? isSubmit,
     bool? isLoginCountLimit,
   }) {
     return LoginState(
       loadLoginStatus: loadLoginStatus ?? this.loadLoginStatus,
+      loginStatus: loginStatus ?? this.loginStatus,
       isSubmit: isSubmit ?? this.isSubmit,
       isLoginCountLimit: isLoginCountLimit ?? this.isLoginCountLimit,
     );
